8e8c4495bf64025ab5cab97dd58c8cb4a50a22dc
Sergio Conti <seracon@gmail.com>
2024-12-08 10:37:39 -0300

    Update and adding new classes
    Adding new functions and new classes in the dependencies

--- main.js
+++ main.js
@@ -1,3 +1,11 @@
- const web = require('./module/myModule')
+ /*const web = require('./module/myModule')
 
-console.log(web)
+console.log(web)*/
+
+
+const {myArray, myNumber, myWebAddrress, user} = require('./module/myModule');
+
+console.log(myArray);
+console.log(myNumber);
+console.log(myWebAddrress);
+console.log(user);
--- module/myModule.js
+++ module/myModule.js
@@ -1,5 +1,17 @@
 const myWebAddrress = "faztweb.com.ar"
+const myNumber = 30
+const myArray = [10, 20, 30]
+const user = {
+    name: 'ryan',
+    lastname: 'ray'
+}
 
-module.exports = myWebAddrress
+module.exports =  { 
+    myWebAddrress,
+    myNumber,
+    myArray,
+    user
+} 
 
-console.log(module)
+
+//console.log(module)
